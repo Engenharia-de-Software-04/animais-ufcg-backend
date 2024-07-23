9ece3901f343306bf6cd31a591b08e1fb0ac958c
wellyremigio <welly.remigio5@gmail.com>
2024-07-23 20:41:24 -0300

    creation of project

---
+++ src/test/java/br/ufcg/animais/animais_ufcg/AnimaisUfcgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufcg.animais.animais_ufcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimaisUfcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
