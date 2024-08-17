963cb8525920f1d233585928b41fd06b6b659895
Vitoria-Maria0912 <vitoria.nascimento@ccc.ufcg.edu.br>
2024-08-16 22:38:46 -0300

    feat: Creating Animals controller.

---
+++ src/main/java/br/ufcg/animais/animais_ufcg/controllers/AnimalsController.java
@@ -0,0 +1,15 @@
+package br.ufcg.animais.animais_ufcg.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public class AnimalsController {
+    
+    @PostMapping()
+    public ResponseEntity<?> creatingAnimal(@RequestBody @Valid AnimalPostPutRequestDTO animalPostPutRequestDto) {
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(AnimalService.creatingAnimal(animalPostPutRequestDto));
+    }    
+}
