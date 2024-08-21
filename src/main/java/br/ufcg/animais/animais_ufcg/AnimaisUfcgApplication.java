79103dced1aa63fd92972c52e51e169766fc97d4
wellyremigio <welly.remigio5@gmail.com>
2024-08-21 01:03:34 -0300

    feat: integrating with mongodb

--- src/main/java/br/ufcg/animais/animais_ufcg/AnimaisUfcgApplication.java
+++ src/main/java/br/ufcg/animais/animais_ufcg/AnimaisUfcgApplication.java
@@ -2,8 +2,10 @@ package br.ufcg.animais.animais_ufcg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class AnimaisUfcgApplication {
 
 	public static void main(String[] args) {
