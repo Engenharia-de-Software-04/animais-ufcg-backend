fa3c8036f39c8829821c6e09e701e90205af4818
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2024-09-11 22:49:08 -0300

    fix: AnimalNotExistException

--- src/main/java/br/ufcg/animais/animais_ufcg/exception/AnimalNotExist.java
+++ src/main/java/br/ufcg/animais/animais_ufcg/exception/AnimalNotExist.java
@@ -1,3 +1,8 @@
-public class AnimalNotExist {
+import br.ufcg.animais.animais_ufcg.AnimaisufcgException;
+
+public class AnimalNotExist extends AnimaisufcgException{
+    public AnimalNotExist(){
+        super("Animal não cadastrado no sistema!");
+    }
     
 }
