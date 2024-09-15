8daab44f331723e77e7e60d03369c6c3c8b90879
wellyremigio <welly.remigio5@gmail.com>
2024-09-15 17:41:49 -0300

    fix: altering the route to no authentication

--- src/main/java/br/ufcg/animais/animais_ufcg/infra/security/SecurityConfig.java
+++ src/main/java/br/ufcg/animais/animais_ufcg/infra/security/SecurityConfig.java
@@ -31,8 +31,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login", "auth/register").permitAll()
-                        //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/h2-console").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/h2-console", "/animal/getAvailable").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
