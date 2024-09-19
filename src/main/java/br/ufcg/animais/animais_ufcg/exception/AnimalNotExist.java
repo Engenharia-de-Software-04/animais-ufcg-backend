d7a98a30c6eae131813a010c9dd12f2526ca9fe9
venanciojamilly <jamilly.silva@ccc.ufcg.edu.br>
2024-09-18 22:56:22 -0300

    fix: directory exception and tests

--- src/main/java/br/ufcg/animais/animais_ufcg/exception/AnimalNotExist.java
+++
@@ -1,9 +0,0 @@
-package br.ufcg.animais.animais_ufcg.exception;
-
-import br.ufcg.animais.animais_ufcg.exception.*;
-public class AnimalNotExist extends AnimaisufcgException{
-    public AnimalNotExist(){
-        super("Animal doesnt exist!");
-    }
-    
-}
